7d9a05425ab42cbd494aa9ae77a965d355d503bd
Nathan Hui <nthui@ucsd.edu>
2024-09-23 17:38:13 -0700

    feat: Adds auto deploy, adds docker compose

---
+++ deploy_ansible.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd /home/waiter/waiter
+
+# Change to main branch if needed
+BRANCH="$(git branch --show-current)"
+if [ "$BRANCH" != "main" ]
+then
+  git checkout main -q
+fi
+
+# Deploy if changes have been made
+GIT_STATUS="$(git status -s)"
+if [ -n "$GIT_STATUS" ]
+then
+    source .venv/bin/activate
+    ansible-galaxy collection install -r requirements.yml
+    ansible-playbook playbook.yaml
+    docker compose pull
+    docker compose up --detach --remove-orphans
+    docker image prune
+fi
