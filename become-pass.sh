c48be606080d25a38cbea2312cebce2ee2737570
Nathan Hui <nthui@ucsd.edu>
2024-09-26 12:56:52 -0700

    fix: Fixes env

--- become-pass.sh
+++ become-pass.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-BW_SESSION=`cat /home/waiter-admin/bw_session`
 date >> ./become-pass.log
 env >> ./become-pass.log
-/home/waiter-admin/.nvm/versions/node/v22.9.0/bin/node /home/waiter-admin/.nvm/versions/node/v22.9.0/lib/node_modules/@bitwarden/cli/build/bw.js get password 'waiter-admin'
+env BW_SESSION=`cat /home/waiter-admin/bw_session` /home/waiter-admin/.nvm/versions/node/v22.9.0/bin/node /home/waiter-admin/.nvm/versions/node/v22.9.0/lib/node_modules/@bitwarden/cli/build/bw.js get password 'waiter-admin'
