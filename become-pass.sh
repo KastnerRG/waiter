f0a56dec6056df6dc3c47fac700e51b74351e42f
Nathan Hui <nthui@ucsd.edu>
2024-09-26 09:07:31 -0700

    fix: Grabs session key

--- become-pass.sh
+++ become-pass.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -e
+BW_SESSION=`cat /home/waiter-admin/bw_session`
 /home/waiter-admin/.nvm/versions/node/v22.9.0/bin/node /home/waiter-admin/.nvm/versions/node/v22.9.0/lib/node_modules/@bitwarden/cli/build/bw.js get password 'waiter-admin'
