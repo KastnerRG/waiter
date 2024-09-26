db874439a89a5e9a2da97cef84ebb9f51c88d3e4
Nathan Hui <nthui@ucsd.edu>
2024-09-26 12:57:50 -0700

    fix: Fixes password scripts

--- become-pass.sh
+++ become-pass.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 set -e
-date >> ./become-pass.log
-env >> ./become-pass.log
 env BW_SESSION=`cat /home/waiter-admin/bw_session` /home/waiter-admin/.nvm/versions/node/v22.9.0/bin/node /home/waiter-admin/.nvm/versions/node/v22.9.0/lib/node_modules/@bitwarden/cli/build/bw.js get password 'waiter-admin'
