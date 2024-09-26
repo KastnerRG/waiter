70a9c9606ab328425b11c3dc34ff5e9361069e6b
Nathan Hui <nthui@ucsd.edu>
2024-09-26 07:55:23 -0700

    fix: Adds login env

--- become-pass.sh
+++ become-pass.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-PATH=/home/waiter-admin/.nvm/versions/node/v22.9.0/bin/:$PATH
+source /home/waiter-admin/.bashrc
 /home/waiter-admin/.nvm/versions/node/v22.9.0/bin/bw get password 'waiter-admin'
