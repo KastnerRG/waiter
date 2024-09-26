99ef96b4f336a1631996604f4e2fb1a121c30074
Nathan Hui <nthui@ucsd.edu>
2024-09-26 09:06:11 -0700

    fix: Fixes node invocation

--- become-pass.sh
+++ become-pass.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -e
-/home/waiter-admin/.nvm/versions/node/v22.9.0/bin/node /home/waiter-admin/.nvm/versions/node/v22.9.0/bin/bw get password 'waiter-admin'
+/home/waiter-admin/.nvm/versions/node/v22.9.0/bin/node /home/waiter-admin/.nvm/versions/node/v22.9.0/lib/node_modules/@bitwarden/cli/build/bw.js get password 'waiter-admin'
