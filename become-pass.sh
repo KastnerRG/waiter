9ee9538da6aee01a31c9760e6b79c4772d11f087
Nathan Hui <nthui@ucsd.edu>
2024-09-26 09:04:01 -0700

    fix: Using explicit node invocation

--- become-pass.sh
+++ become-pass.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 set -e
-/home/waiter-admin/.nvm/nvm.sh exec 22.9.0
-/home/waiter-admin/.nvm/versions/node/v22.9.0/bin/bw get password 'waiter-admin'
+/home/waiter-admin/.nvm/versions/node/v22.9.0/bin/node /home/waiter-admin/.nvm/versions/node/v22.9.0/bin/bw get password 'waiter-admin'
