0114836cb04f2785a8253c56b5ef4d02aa0edaf9
Nathan Hui <nthui@ucsd.edu>
2024-10-08 10:09:34 -0700

    fix: Fixes logging and images

--- deploy_ansible.sh
+++ deploy_ansible.sh
@@ -21,7 +21,7 @@ then
     ANSIBLE_INVENTORY='/home/waiter-admin/waiter/inventory.yaml'
     BW_SESSION=`cat /home/waiter-admin/bw_session`
     ansible-playbook playbook.yaml
-    docker compose pull >> docker_compose.log 2&>1
-    docker compose up --detach >> docker_compose.log 2&>1
-    docker image prune -f >> docker_compose.log 2&>1
+    docker compose pull |& tee -a docker_compose.log
+    docker compose up --detach |& tee -a docker_compose.log
+    docker image prune -f |& tee -a docker_compose.log
 fi
