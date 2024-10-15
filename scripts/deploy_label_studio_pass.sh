8c6cad7e8d8fc0d357ffd48a891070a5dca29092
Nathan Hui <nthui@ucsd.edu>
2024-10-15 15:26:51 -0700

    feat: Switches to script to make password

---
+++ scripts/deploy_label_studio_pass.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+NODE=$HOME/.nvm/versions/node/v22.9.0/bin/node
+BW=$HOME/.nvm/versions/node/v22.9.0/lib/node_modules/@bitwarden/cli/build/bw.js
+BW_SESSION=`cat .bw_session`
+LABEL_ADMIN_PASSWD=`$NODE $BW --session $BW_SESSION get password "E4E Label Studio Admin Password"`
+echo 'LABEL_STUDIO_PASSWORD=$LABEL_ADMIN_PASSWD' > $HOME/waiter/.secrets/label_studio_admin_password.env
